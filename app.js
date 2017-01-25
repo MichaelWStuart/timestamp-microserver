b3da13636348323b599da903d2c35ba32a65b02b
Michael Stuart <mikewstuart@gmail.com>
2017-01-24 21:14:35 -0800

    use env port for heroku

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const time = require('./time');
 const path = require('path');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
 app.get('/:input', (req,res) => {
   try {
@@ -14,6 +15,8 @@ app.get('/:input', (req,res) => {
     console.error(e)
     res.sendStatus(400);
   }
-})
+});
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
+});
